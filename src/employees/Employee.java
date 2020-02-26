4e81d4eed917c411fa55a2c84ec6b51911e3efc6
Kenta Shikata <shikatakenta@gmail.com>
2020-02-26 08:37:03 +0100

    first commit

---
+++ src/employees/Employee.java
@@ -0,0 +1,70 @@
+package employees;
+
+public class Employee {
+        
+        int id;
+	String firstName;
+	String lastName;
+        String email;
+        String phone;
+
+    public Employee(int id, String firstName, String lastName, String email, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    Employee() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+        
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + '}';
+    }
+	
+        
+}
---
+++ src/employees/Service.java
@@ -0,0 +1,27 @@
+package employees;
+
+import static com.sun.xml.internal.ws.api.message.Packet.Status.Response;
+import javax.xml.ws.Response;
+
+
+
+public class Service {
+
+	
+	public Employee getEmployeeInJSON() {
+
+		Employee e1 = new Employee(1,"Kenta","Shikata","shikata@gmail.com","0551");
+		return e1; 
+
+	}
+
+
+	public Response createEmployeeInJSON(Employee e) {
+
+		String result = "Employee created : " + e;
+            return null;
+	
+		
+	}
+	
+}
