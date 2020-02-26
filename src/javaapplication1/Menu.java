aa25f97080fbec948b92dbeb11e1bd6afd65bd36
Kenta Shikata <shikatakenta@gmail.com>
2020-02-26 10:48:14 +0100

    get id, delete

--- src/javaapplication1/Menu.java
+++ src/javaapplication1/Menu.java
@@ -11,77 +11,60 @@ import java.util.Scanner;
  *
  * @author informatica
  */
-class Menu{
-public static void main(String args[]){
-System.out.println("Scegli il comando: ");
-System.out.println("a: Get all Employees");
-System.out.println("b: Get Employee");
-System.out.println("c: Create Employee");
-System.out.println("d: Delete Employee");
-System.out.println("e: Patch Employee");
-System.out.println("f: Update Employee");
-System.out.println("?: Display");
-System.out.println("q: Quit");
-Scanner scan = new Scanner(System.in);
-String choice = scan.nextLine();
-do {
-switch (choice){
-    case "a":
-     Get.Get();
-        break;
-    case "b":
-     
-        break;
-    case "c":
-        System.out.println("Id: ");
-        int input1 = scan.nextInt();   
-        System.out.println("First Name: ");
-        scan.next();
-        String input2 = scan.nextLine();
-        System.out.println("Last Name: ");
-         scan.next();
-        String input3 = scan.nextLine();
-         scan.next();
-        System.out.println("Email: ");  
-        String input4 = scan.nextLine();
-         scan.next();
-        System.out.println("Phone: ");
-         scan.next();
-        String input5 = scan.nextLine();
-    String input = "{\"employeeId\":"+input1+",\n" +
-"  \"firstName\": \"Kenta\",\n" +
-"  \"lastName\": \"Shikata\",\n" +
-"  \"email\": \"shikatakenta@gmail.com\",\n" +
-"  \"phone\": \"222\"\n" +
-"}";
-        System.out.println(input);
-        Post.Create(input);
-        break;
-
-
-
-
-    case "d":
- 
-        break;
-    case "e":
-
-        break;
-    case "f":
-
-        break;
-    case "?":
-            System.out.println("Command Options: ");
-            System.out.println("a: Geek's Name");
-            System.out.println("b: Num Questions Asked");
-            System.out.println("c: All Numbers Are the Same");
-            System.out.println("d: Sum Between Two Integers");
-            System.out.println("e: Repeat the String");
-            System.out.println("f: It is Palindrome");
+class Menu
+{
+    
+    public static void main(String args[])
+    {
+        String choice = "-1";
+        
+        do
+        {
+            System.out.println("Scegli il comando: ");
+            System.out.println("a: Get all Employees");
+            System.out.println("b: Get Employee");
+            System.out.println("c: Create Employee");
+            System.out.println("d: Delete Employee");
+            System.out.println("e: Patch Employee");
+            System.out.println("f: Update Employee");
             System.out.println("?: Display");
             System.out.println("q: Quit");
-            break;
-        }  }while (choice != "q");
+            Scanner scan = new Scanner(System.in);
+            choice = scan.nextLine();
 
-}
+            switch (choice)
+            {
+                case "a":
+                 Methods.Get();
+                    break;
+                case "b":
+                Methods.GetId();
+                    break;
+                case "c":
+                Methods.Create();
+                    break;
+
+                case "d":
+                Methods.Delete();
+                    break;
+                case "e":
+
+                    break;
+                case "f":
+
+                    break;
+                case "?":
+                    System.out.println("Command Options: ");
+                    System.out.println("a: Geek's Name");
+                    System.out.println("b: Num Questions Asked");
+                    System.out.println("c: All Numbers Are the Same");
+                    System.out.println("d: Sum Between Two Integers");
+                    System.out.println("e: Repeat the String");
+                    System.out.println("f: It is Palindrome");
+                    System.out.println("?: Display");
+                    System.out.println("q: Quit");
+                    break;
+            }
+        }while (!choice.equals("q"));
+    }
 }
---
+++ src/javaapplication1/Methods.java
@@ -0,0 +1,160 @@
+package javaapplication1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class Methods
+{
+
+
+	//  http://localhost:8080/api/tutorial/1.0/employees
+
+            public static void Create() {
+            try {
+
+		URL url = new URL(" http://localhost:8080/api/tutorial/1.0/employees");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setDoOutput(true);
+		conn.setRequestMethod("POST");
+		conn.setRequestProperty("Content-Type", "application/json");
+		String input = "{\"employeeId\":" + 420+ "," + "\"firstName\":" + "\"" + "Richard" + "\"" + "," + "\"lastName\":" + "\"" + "Gut" + "\"" + "," + "\"email\":" + "\"" + "Richard@gm.com" + "\"" + "," + "\"phone\":" + "\"" + "5050" + "\"" + "}";
+
+
+		OutputStream os = conn.getOutputStream();
+		os.write(input.getBytes());
+		os.flush();
+		if (conn.getResponseCode() != 200 && conn.getResponseCode() != 201) {
+			throw new RuntimeException("Failed : HTTP error code : "
+				+ conn.getResponseCode());
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+				(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+
+	 }
+
+	}
+           
+            
+        
+        public static void Get() {
+
+	  try {
+
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+	  }
+	}    
+        
+        
+        
+        public static void GetId() {
+
+	  try {
+
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees/8");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+	  }
+	}    
+        
+        public static void Delete() {
+
+	  try {
+
+		URL url = new URL("http://localhost:8080/api/tutorial/1.0/employees/8");
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("DELETE");
+		conn.setRequestProperty("Accept", "application/json");
+
+		if (conn.getResponseCode() != 200) {
+			throw new RuntimeException("Failed : HTTP error code : "+ conn.getResponseCode());
+		}
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(
+			(conn.getInputStream())));
+
+		String output;
+		System.out.println("Output from Server .... \n");
+		while ((output = br.readLine()) != null) {
+			System.out.println(output);
+		}
+
+		conn.disconnect();
+	  } catch (MalformedURLException e) {
+
+		e.printStackTrace();
+
+	  } catch (IOException e) {
+
+		e.printStackTrace();
+	  }
+	}    
+            
+}
