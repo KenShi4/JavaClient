a23823d67f253195d1058944ad5388325bb9541a
Kenta Shikata <shikatakenta@gmail.com>
2020-03-04 08:48:33 +0100

    modifiche menu

--- src/javaapplication1/Menu.java
+++ src/javaapplication1/Menu.java
@@ -48,19 +48,19 @@ class Menu
                 Methods.Delete();
                     break;
                 case "e":
-
+                //Methods.Update();
                     break;
                 case "f":
-Methods.Update();
+                Methods.Update();
                     break;
                 case "?":
-                    System.out.println("Command Options: ");
-                    System.out.println("a: Geek's Name");
-                    System.out.println("b: Num Questions Asked");
-                    System.out.println("c: All Numbers Are the Same");
-                    System.out.println("d: Sum Between Two Integers");
-                    System.out.println("e: Repeat the String");
-                    System.out.println("f: It is Palindrome");
+                    System.out.println("Scegli il comando: ");
+                    System.out.println("a: Get all Employees");
+                    System.out.println("b: Get Employee");
+                    System.out.println("c: Create Employee");
+                    System.out.println("d: Delete Employee");
+                    System.out.println("e: Patch Employee");
+                    System.out.println("f: Update Employee");
                     System.out.println("?: Display");
                     System.out.println("q: Quit");
                     break;
